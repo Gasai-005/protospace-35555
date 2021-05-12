0ebe87f9679fdbcd0f06e9249bd96c184ee1a7fb
Gasai-005 <82623062+Gasai-005@users.noreply.github.com>
2021-05-12 11:27:12 +0900

    Issueの解決

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
       redirect_to prototype_path(@comment.prototype)
     else
       @prototype = @comment.prototype
-      @comments = Comment.new
+      @comments = Comment.all
       render "prototypes/show"
     end
   end
--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -21,7 +21,7 @@ class PrototypesController < ApplicationController
   def show
     @prototype = Prototype.find(params[:id])
     @comment = Comment.new
-    @comments = @prototype.comments.all
+    @comments = Comment.all
   end
 
   def edit
